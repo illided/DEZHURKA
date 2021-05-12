d6db27be4f12a0746f91d40ef2ef5fdbef1f260c
illided <utkin.ilya.nickolaevich@gmail.com>
2021-05-12 13:55:51 +0300

    logic tables added

---
+++ postgress/game_logic.sql
@@ -0,0 +1,34 @@
+create table unlockable_workers
+(
+    id            SERIAL PRIMARY KEY,
+    type          worker_type NOT NULL,
+    surname       varchar(50) NOT NULL,
+    name          varchar(50) NOT NULL,
+    patronymic    varchar(50),
+    qualification integer     NOT NULL,
+    UNIQUE (type, surname, name, patronymic)
+);
+
+create table possible_tasks
+(
+    id           SERIAL PRIMARY KEY,
+    service_type varchar(400) NOT NULL,
+    description  varchar(400) NOT NULL,
+    difficulty   integer      not null,
+    room_id      integer references rooms (id) on delete cascade
+);
+
+create table possible_services
+(
+    service_id  SERIAL PRIMARY KEY,
+    description varchar(400) NOT NULL UNIQUE,
+    worker_type worker_type  NOT NULL
+);
+
+create table possible_buildings
+(
+    address varchar(400),
+    type    buildings_types
+);
+
+
